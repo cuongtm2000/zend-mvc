9fb0f2a4286c1afd8eec7a0fa5ce1ca55ae9096a
khangninh2005@gmail.com <khangninh2005@gmail.com@8895e86c-fc54-b5c1-1ef4-2b8eb979d5c2>
2011-11-23 13:00:32 +0000

    Đã chỉnh sửa theo yêu cầu ngày 23/11/2011

--- trunk/saigonsoft/application/modules/solution/models/SolutionCat.php
+++ trunk/saigonsoft/application/modules/solution/models/SolutionCat.php
@@ -40,7 +40,11 @@ class Solution_Model_SolutionCat extends Zend_Db_Table{
     
 	public function listCatParent($cid = 0){
 		try {
-			$select = $this->select()->from($this->_name, array('cat_id', 'cat_title'.LANG, 'cat_parent_id', 'cat_order', 'cat_enable'))->order('cat_order DESC');
+			$select = $this->select()
+							->from($this->_name, 
+									array('cat_id', 'cat_title'.LANG, 'cat_parent_id', 
+											'cat_order', 'cat_enable'))
+							->order('cat_order DESC');
 			$result = $this->fetchAll($select);
 	    	if(count($result)){
 	    		$this->_catdata = $result->toArray();
