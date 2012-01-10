f519c06b445ee050f5bf26e3c21df0020dba5f7a
info@grouplaptrinh.com <info@grouplaptrinh.com@8895e86c-fc54-b5c1-1ef4-2b8eb979d5c2>
2012-01-10 13:35:55 +0000

    Sua app-rotues.ini

--- trunk/mvc/define.php
+++ trunk/mvc/define.php
@@ -15,8 +15,5 @@ define('TEMPLATE_DIR', '/public/templates');
 define('TEM_INDEX', '/thanda'); //Template Front end
 define('TEM_ADMIN', '/webadmin'); //Template Back end
 
-
-//define('REAL_PATH', realpath(dirname(__FILE__)));
 define('PUBLIC_PATH', realpath(dirname(__FILE__) . '/public'));
-define('IMAGES_PATH',  '/public/userfiles/images/');
 define('TEMPLATE_PATH', PUBLIC_PATH .'/templates'. TEM_INDEX);
