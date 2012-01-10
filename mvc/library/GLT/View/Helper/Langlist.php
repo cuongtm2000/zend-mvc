5c01f2611ac70f2b518e815726c1fbf4c0fd35ec
info <info@grouplaptrinh.com>
2012-01-10 13:35:55 +0000

    Sua app-rotues.ini

--- mvc/library/GLT/View/Helper/Langlist.php
+++ mvc/library/GLT/View/Helper/Langlist.php
@@ -1,13 +1,10 @@
 <?php
 class GLT_View_Helper_Langlist extends Zend_View_Helper_Abstract{
     public function langlist(){
-        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/app-router.ini', 'lang');
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/app-router.ini');
         $langinput = $config->routes->language->reqs->language;
-        
-       // $langinput = str_replace("kr", "fr", $langinput);
-                
         $langdefault = $config->routes->language->defaults->language;
-        //var_dump($langdefault);
+        
         $start = '^(';
         $end = ')$';
         $substr = substr($langinput, strlen($start)+strpos($langinput, $start), (strlen($langinput) - strpos($langinput, $end))*(-1));
